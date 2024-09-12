d83a4729c28f3ec11bdf0634cb499502cd9363c0
Jinsoo Park <jinny.park908@gmail.com>
2024-09-12 22:30:08 +0900

    ✨feat: BOJ_2178 미로탐색 알고리즘 구현

--- week2/BOJ_2178_미로탐색/jinsoo.py
+++ week2/BOJ_2178_미로탐색/jinsoo.py
@@ -19,7 +19,7 @@ def bfs(x,y):
 
     while queue:
         x, y = queue.popleft()
-        for i in range(4):
+        for i in range(4): 
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < N and 0<= ny < M:
