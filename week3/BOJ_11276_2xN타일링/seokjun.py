147529e2665503efb0dbd4c213ce42dee422b4f9
Seokjun Kang <kangsj0327@gmail.com>
2024-09-23 00:55:06 +0900

    [fix] 2xN 타일링 인덱스 오류 수정

--- week3/BOJ_11276_2xN타일링/seokjun.py
+++ week3/BOJ_11276_2xN타일링/seokjun.py
@@ -4,6 +4,13 @@ input = sys.stdin.readline
 
 N = int(input().strip())
 
+if N == 1:
+  print(1)
+  sys.exit(0)
+if N == 2:
+  print(2)
+  sys.exit(0)
+
 dp = list(0 for _ in range(N + 1))
 dp[1] = 1
 dp[2] = 2
