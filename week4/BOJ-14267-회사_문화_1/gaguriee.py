35b85a1af45a47770d54a03de6b9c1fa22b4d4e7
gaguriee <74501631+gaguriee@users.noreply.github.com>
2024-09-28 17:26:43 +0900

    ✨feat: BOJ_14267_회사문화1 알고리즘 구현

---
+++ week4/BOJ-14267-회사_문화_1/gaguriee.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+# 회사의 직원 수 n명, 최초의 칭찬의 횟수 m
+N, M = map(int, input().split())
+
+# 직원 n명의 직속 상사의 번호 (boss[i]는 i번의 상사 번호)
+boss = [0] + list(map(int, input().split())) 
+
+# 직원별 칭찬 횟수
+dp = [0] * (N+1)
+
+for _ in range(M):
+    i, w = map(int, input().split())
+    dp[i] = dp[i] + w
+
+for i in range(2,N+1,1):
+    # 직속상사의 내리칭찬
+    dp[i] = dp[boss[i]] + dp[i]
+
+# 0번째 인덱스 제외
+print(" ".join(map(str, dp[1:])))
