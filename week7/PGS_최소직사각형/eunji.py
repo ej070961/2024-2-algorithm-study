40969782a0ab922a8b9095978e7ce02fe54a7412
ej070961 <ej070961@gmail.com>
2024-10-16 10:49:44 +0900

    ♻️ refactor: PGS_최소직사각형 정답코드로 수정

--- week7/PGS_최소직사각형/eunji.py
+++ week7/PGS_최소직사각형/eunji.py
@@ -1,20 +1,17 @@
-import sys
+def solution(sizes):
+  
+    max_w = 0
+    max_h = 0
 
-input = sys.stdin.readline
+    for i in range(len(sizes)):
+        x, y = map(int, sizes[i])
+        w = max(x, y)
+        h = min(x, y)
 
-
-sizes = eval(input().rstrip())
-
-max_w = 0
-max_h = 0
-
-for i in range(len(sizes)):
-    x, y = map(int, sizes[i])
-    w = max(x, y)
-    h = min(x, y)
-
-    max_w = max(max_w, w)
-    max_h = max(max_h, h)
+        max_w = max(max_w, w)
+        max_h = max(max_h, h)
 
 
-print(max_w*max_h)
+    answer = max_w*max_h
+
+    return answer
