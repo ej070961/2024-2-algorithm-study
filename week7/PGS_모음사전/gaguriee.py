b03a666081767075141ea193f1ca81a73ded51bd
gaguriee <74501631+gaguriee@users.noreply.github.com>
2024-10-17 23:12:03 +0900

    chore: PGS_모음사전 알고리즘 주석추가

--- week7/PGS_모음사전/gaguriee.py
+++ week7/PGS_모음사전/gaguriee.py
@@ -3,6 +3,9 @@ def solution(word):
     
     chars = ['A', 'E', 'I', 'O', 'U']
     
+    # 자릿수별 가능한 단어 수
+    # 첫번째 자리가 E (index 2)로 시작하면, A (index 1)로 시작하는 모든 단어 건너띔 
+    # (5^4 + 5^3 + 5^2 + 5^1 + 5^0  =  625 + 125 + 25 + 5 + 1 = 781)
     weights = [781, 156, 31, 6, 1]
     
     for i, char in enumerate(word):
