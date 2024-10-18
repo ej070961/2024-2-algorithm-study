e57d9d5efca167863c253252c51d4c9a94f6857c
ej070961 <ej070961@gmail.com>
2024-10-18 09:23:25 +0900

    ✨feat: PGS_피로도 알고리즘 구현

---
+++ week7/PGS_피로도/eunji.py
@@ -0,0 +1,28 @@
+# 최소 필요 피로도: 해당 던전을 탐험하기 위해 가지고 있어야 하는 최소한의 피로도
+# 소모 피로도: 던전을 탐험한 후 소모되는 피로도
+# 현재 피로도 k와 각 던전별 "최소 필요 피로도", "소모 피로도"가 담긴 2차원 배열 dungeons 가 매개변수로 주어질 때, 
+# 유저가 탐험할수 있는 최대 던전 수를 return 하도록 solution 함수를 완성
+
+visited = []
+answer = 0
+#피로도, 던전 방문 수, 던전 배열
+def dfs(k, cnt, arr):
+    global answer
+
+    if cnt > answer:
+        answer = cnt 
+    for j in range(len(arr)):
+        if k >= arr[j][0] and not visited[j]:
+            visited[j] = True
+            dfs(k-arr[j][1], cnt + 1, arr)
+            visited[j] = False #백트래킹: 방문하지 않은 상태로 되돌림 다른 경우의 수를 탐색하기 위해 
+
+def solution(k, dungeons):
+    global visited
+    #dfs를 위한 방문 배열 생성 
+    visited = [False] * len(dungeons)
+
+    dfs(k, 0, dungeons)
+    return answer
+
+print(solution(80,[[80,20],[50,40],[30,10]] ))
